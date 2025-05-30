5626ea5dcd2a3793fd1489160d249d96cd87f447
s.dhayu <kutnej.santos@escola.pr.gov.br>
2025-05-30 16:05:17 -0300

    z

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ function criaCartao(categoria, pergunta, resposta) {
 
     let respostaEstaVisivel = false
 
-    function viraCartao() {
+    function viraCartao ()  {
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
+
     }
+
     cartao.addEventListener('click', viraCartao)
 
 
     container.appendChild(cartao)
-
 }
